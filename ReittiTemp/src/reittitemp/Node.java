5042b337ca248700135556e01b6fa65ab51cd6d0
pumm1 <patsu01@gmail.com>
2017-11-05 08:34:58 +0200

    pientä koodailua

--- ReittiTemp/src/reittitemp/Node.java
+++ ReittiTemp/src/reittitemp/Node.java
@@ -6,11 +6,16 @@ verkon solmujen toteutus
 public class Node {
 
     protected Node[] neighbours;
+    protected int nodes;
+    protected int color; //0=ei tutkittu, 1 = tutkittu
+    protected int distance;
 
     public Node() {
         //naapureita voi olla maksimissaan 4 ruudukossa (vertikaalinen ja horisontaalinen liike)
-        neighbours = new Node[4]; 
-
+        neighbours = new Node[4];
+        nodes = 0;
+        color = 0;
+        distance = 99999;
     }
 
     //lisätään solmulle naapuri
@@ -25,8 +30,26 @@ public class Node {
         }
 
     }
-    
-    public Node[] getNeighbours(){
+
+    public void connect(Node n) {
+        int temp = 0;
+        for (int i = 0; i < 4; i++) {
+            if (neighbours[i] == n) {
+                temp++;
+                break;
+            }
+        }
+        if (temp == 0) {
+            neighbours[nodes++] = n;
+            n.connect(this);
+        }
+    }
+
+    public void visit() {
+        color = 1;
+    }
+
+    public Node[] getNeighbours() {
         return neighbours;
     }
 }
