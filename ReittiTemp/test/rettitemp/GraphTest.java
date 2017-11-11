ea2b6596b281bec5f65d728d2f65a96f8552b703
pumm1 <patsu01@gmail.com>
2017-11-11 14:29:16 +0200

    BFS automaattista testiä

--- ReittiTemp/test/rettitemp/GraphTest.java
+++ ReittiTemp/test/rettitemp/GraphTest.java
@@ -1,5 +1,6 @@
 package rettitemp;
 
+import java.util.HashMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,14 +10,12 @@ import static org.junit.Assert.*;
 import reittitemp.Graph;
 import reittitemp.Node;
 
-
 public class GraphTest {
 
     private Graph g;
     private int[][] grid;
     private Node[][] nGrid;
     private Node n;
-    
 
     @Before
     public void setUp() {
@@ -27,7 +26,7 @@ public class GraphTest {
         g = new Graph(grid);
         g.initGraph();
         nGrid = g.getNodeGrid();
-        
+
     }
 
     @Test
@@ -47,6 +46,22 @@ public class GraphTest {
             }
         }
         assertEquals(2497, maara);
+    }
+
+    @Test
+    public void BFStoimii() {
+
+        Node s = nGrid[1][0];
+        HashMap<Node, Node> tree = g.BFS(s);
+        Node u = nGrid[0][3];
+        u = tree.get(u);
+        assertEquals(nGrid[1][3], u);
+        u = tree.get(u);
+        assertEquals(nGrid[1][2], u);
+        u = tree.get(u);
+        assertEquals(nGrid[1][1], u);
+        u = tree.get(u);
+        assertEquals(s, u);
     }
 
 }
